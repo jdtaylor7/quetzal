eb353b242b92995c451d6411e47cc5f344b28b68
Jordan Taylor <jdtaylor7@gmail.com>
2020-12-07 20:15:28 -0800

    Add serial printing capability.

---
+++ include/serial.hpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#include <stm32f1xx.h>
+
+constexpr uint32_t HSI = 8000000;  // High-speed internal clock
+
+void init_serial(uint32_t baud)
+{
+    uint16_t usart_divider = HSI / (16 * baud);
+
+    // Set up USART1 peripheral.
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;  // Enable USART1 peripheral
+    USART1->CR1 = USART_CR1_UE | USART_CR1_TE;
+    USART1->CR2 = 0;
+    USART1->CR3 = 0;
+    USART1->BRR = usart_divider << 4;
+
+    // Set up GPIOA 9 for USART1 TX alternative function.
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    GPIOA->CRH |= GPIO_CRH_MODE9_1 | GPIO_CRH_CNF9_1;
+    GPIOA->CRH &= ~(GPIO_CRH_MODE9_0 | GPIO_CRH_CNF9_0);
+}
+
+void usartWriteChar(unsigned char c)
+{
+    while (!(USART1->SR & USART_SR_TXE)) {}
+    USART1->DR = c;
+}
+
+void usartWriteStr(const std::string& str)
+{
+    for (unsigned char c : str)
+        usartWriteChar(c);
+}
+
+// void usartWriteStr(const char* str)
+// {
+//     for (const char* c = str; *c; c++)
+//         usartWriteChar(*c);
+// }
--- src/quetzal.cpp
+++ src/quetzal.cpp
@@ -1,26 +1,40 @@
 #include <stm32f1xx.h>
 
+#include "serial.hpp"
+
+constexpr uint16_t USART_BAUD = 9600;
+
+void blink()
+{
+    int ctr;
+    ctr = (8000000 / 3) / 2 / 1;
+    // each loop iteration takes 3 cycles to execute.
+    while (ctr) {
+        asm ("");
+        --ctr;
+    }
+    GPIOC->BRR = 1 << 13;
+    ctr = (8000000 / 3) / 2 / 1;
+    // each loop iteration takes 3 cycles to execute.
+    while (ctr) {
+        asm ("");
+        --ctr;
+    }
+    GPIOC->BSRR = 1 << 13;
+}
+
 void mainFn() {
+    // Set up GPIOC 13 (green light) for LED control, then turn off.
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     GPIOC->CRH = 0b0100'0100'0011'0100'0100'0100'0100'0100;
-    GPIOC->BRR = 1 << 13;
+    GPIOC->BSRR = GPIO_BRR_BR13;
 
-    while (1) {
-        int ctr;
-        ctr = (8000000 / 3) / 2 / 1;
-        // each loop iteration takes 3 cycles to execute.
-        while (ctr) {
-            asm ("");
-            --ctr;
-        }
-        GPIOC->BRR = 1 << 13;
-        ctr = (8000000 / 3) / 2 / 1;
-        // each loop iteration takes 3 cycles to execute.
-        while (ctr) {
-            asm ("");
-            --ctr;
-        }
-        GPIOC->BSRR = 1 << 13;
+    init_serial(USART_BAUD);
+
+    while (1)
+    {
+        usartWriteStr("hello!\r\n");
+        blink();
     }
 }
 
